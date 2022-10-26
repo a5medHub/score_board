085b5eb0dd1748ff9ac61e36a791c6f8311c4f27
a5medHub <ayassine.auce@gmail.com>
2022-10-26 07:01:49 +0300

    js

---
+++ index.js
@@ -0,0 +1,8 @@
+let homescore = document.getElementById("home-score");
+
+function scorebuttons1() {
+  homescore = "hello";
+  console.log(homescore);
+}
+scorebuttons1();
+console.log("this is a test");
