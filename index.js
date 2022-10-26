58b6e2c11334a1dd8391910cc6186e60ef7af76c
a5medHub <ayassine.auce@gmail.com>
2022-10-26 07:31:27 +0300

    adding guest js score

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let homescore = document.getElementById("home-score");
-let guestbox = document.getElementById("guest-box");
+let guestscore = document.getElementById("guest-box");
 
 let homeCount1 = 0;
 let homeCount2 = 0;
@@ -10,20 +10,26 @@ let guestCount2 = 0;
 let guestCount3 = 0;
 
 function homeScoreButtons1() {
-  let homeCount1 = Number(homescore.innerText);
+  homeCount1 = Number(homescore.innerText);
   homeCount1 += 1;
   homescore.innerText = homeCount1;
-  console.log(homescore);
+  //console.log(homescore);
 }
 function homeScoreButtons2() {
-  let homeCount2 = Number(homescore.innerText);
+  homeCount2 = Number(homescore.innerText);
   homeCount2 += 2;
   homescore.innerText = homeCount2;
-  console.log(homescore);
+  //console.log(homescore);
 }
 function homeScoreButtons3() {
-  let homeCount3 = Number(homescore.innerText);
-  homeCount3 += 2;
+  homeCount3 = Number(homescore.innerText);
+  homeCount3 += 3;
   homescore.innerText = homeCount3;
-  console.log(homescore);
+  //console.log(homescore);
+}
+
+function guestScoreButtons1() {
+  guestCount1 = Number(document.innerText);
+  guestCount1 += 1;
+  guestscore.innerText = guestCount1;
 }
