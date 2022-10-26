09837943bd171909c153ed3410e9d44c049c3cf8
a5medHub <ayassine.auce@gmail.com>
2022-10-26 07:24:21 +0300

    js button home

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 let homescore = document.getElementById("home-score");
+let guestbox = document.getElementById("guest-box");
 
-function scorebuttons1() {
-  homescore = "hello";
+let homeCount1 = 0;
+let homeCount2 = 0;
+let homeCount3 = 0;
+
+let guestCount1 = 0;
+let guestCount2 = 0;
+let guestCount3 = 0;
+
+function homeScoreButtons1() {
+  let homeCount1 = Number(homescore.innerText);
+  homeCount1 += 1;
+  homescore.innerText = homeCount1;
   console.log(homescore);
 }
-scorebuttons1();
-console.log("this is a test");
+function homeScoreButtons2() {
+  let homeCount2 = Number(homescore.innerText);
+  homeCount2 += 2;
+  homescore.innerText = homeCount2;
+  console.log(homescore);
+}
+function homeScoreButtons3() {
+  let homeCount3 = Number(homescore.innerText);
+  homeCount3 += 2;
+  homescore.innerText = homeCount3;
+  console.log(homescore);
+}
